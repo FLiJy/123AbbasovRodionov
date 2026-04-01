b01a684fff7526e7a9e2f252472ebdeb9dc2b7d9
237127 <237127@KB37-118-C10>
2026-04-01 10:04:38 +0300

    буквы

---
+++ pr7wrds/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/// <summary>
+/// Пример работы с массивом символов и отладкой цикла.
+/// Формирует имя "fred smith" и выводит приветствия.
+/// </summary>
+class ArrayExample
+{
+    static void Main()
+    {
+        char[] letters = { 'f', 'r', 'e', 'd', ' ', 's', 'm', 'i', 't', 'h' };
+        string name = "";
+        int[] a = new int[10];
+
+        for (int i = 0; i < letters.Length; i++)
+        {
+            name += letters[i];
+            a[i] = i + 1;
+            SendMessage(name, a[i]);
+        }
+        Console.ReadKey();
+    }
+
+    /// <summary>
+    /// Выводит приветствие с текущим именем и номером.
+    /// </summary>
+    static void SendMessage(string name, int msg)
+    {
+        Console.WriteLine("Hello, " + name + "! Count to " + msg);
+    }
+}
