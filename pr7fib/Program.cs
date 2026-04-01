784c562efb20429f02644e074d73a1399d274790
237127 <237127@KB37-118-C10>
2026-04-01 09:58:23 +0300

    фибоначи

---
+++ pr7fib/Program.cs
@@ -0,0 +1,16 @@
+﻿static int Fibonacci(int n)
+{
+    Console.WriteLine("The output is: ");
+    int n1 = 0;
+    int n2 = 1;
+    int sum;
+
+    for (int i = 2; i <= n; i++)   // исправлено условие
+    {
+        sum = n1 + n2;
+        n1 = n2;
+        n2 = sum;
+    }
+
+    return n == 0 ? n1 : n2;
+}
